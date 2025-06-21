67a2806d122bd2d7d6561a223005b3e6334aac71
burak <burakkara0090@gmail.com>
2025-06-20 21:00:20 -0500

    burak kara

---
+++ src/com/neotech/lesson1/sample.java
@@ -0,0 +1,20 @@
+package com.neotech.lesson1;
+
+public class sample {
+
+	public static void main(String[] args) {
+	}
+
+	public void test1() {
+		System.out.println("public test!");
+	}
+	
+	public void test2() {
+		System.out.println("public test 2!");
+	}
+
+	public void test3() {
+		System.out.println(" test 3 " );
+		
+	}
+}
