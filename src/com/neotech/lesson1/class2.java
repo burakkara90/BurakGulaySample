1f6b495d0e4289fd43e7372df61287b6c65f0efd
burak <burakkara0090@gmail.com>
2025-06-25 18:29:23 -0500

    first commit

--- src/com/neotech/lesson1/class2.java
+++ src/com/neotech/lesson1/class2.java
@@ -3,8 +3,13 @@ package com.neotech.lesson1;
 public class class2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+System.out.println("java");
 
+
+		
+		
+		
+		
 	}
 
 }
